4ff4198287567253509585679e08acacf4d4bb23
Daniel Hawn <danieljhawn@gmail.com>
2020-09-19 18:16:12 -0500

    5 day forcast works

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const api = {
   key: "a01fee2d80b973ab3b18ce1990905e04",
   base: "https://api.openweathermap.org/data/2.5/",
+  fiveDay: "https://api.openweathermap.org/data/2.5/forecast/"
 }
 
 const defaultCity = "Austin"
@@ -11,11 +12,12 @@ searchbox.addEventListener('keypress', setQuery);
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
+    getFiveDay(searchbox.value);
   }
 }
 
-function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+function getResults(cityName) {
+  fetch(`${api.base}weather?q=${cityName}&units=imperial&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     }).then(displayResults);
@@ -28,10 +30,64 @@ function loadDefaultCity() {
     }).then(displayResults);
 }
 
+function getFiveDay(cityName) {
+  fetch(`${api.fiveDay}?q=${cityName}&units=imperial&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayFiveDay);
+}
+
 loadDefaultCity()
 
-function displayResults(weather) {
+function displayFiveDay(weather) {
   console.log(weather);
+  console.log(weather.list[0].main.temp_max)
+
+  let forecast = document.querySelector('.container');
+  forecast.innerHTML = `
+    <div class="days">
+      <div class="temp">${Math.round(weather.list[0].main.temp)}</div>
+      <div class="weather">${weather.list[0].weather[0].description}</div>
+      <div class="hi-low">${Math.round(weather.list[0].main.temp_min)} / ${Math.round(weather.list[0].main.temp_max)}</div>
+    </div>
+  
+    <div class="days">
+      <div class="temp">${Math.round(weather.list[1].main.temp)}</div>
+      <div class="weather">${weather.list[1].weather[0].description}</div>
+      <div class="hi-low">${Math.round(weather.list[1].main.temp_min)} / ${Math.round(weather.list[0].main.temp_max)}</div>
+    </div>
+  
+    <div class="days">
+      <div class="temp">${Math.round(weather.list[2].main.temp)}</div>
+      <div class="weather">${weather.list[2].weather[0].description}</div>
+      <div class="hi-low">${Math.round(weather.list[2].main.temp_min)} / ${Math.round(weather.list[0].main.temp_max)}</div>
+    </div>
+  
+    <div class="days">
+      <div class="temp">${Math.round(weather.list[3].main.temp)}</div>
+      <div class="weather">${weather.list[3].weather[0].description}</div>
+      <div class="hi-low">${Math.round(weather.list[3].main.temp_min)} / ${Math.round(weather.list[0].main.temp_max)}</div>
+    </div>
+  
+    <div class="days">
+      <div class="temp">${Math.round(weather.list[4].main.temp)}</div>
+      <div class="weather">${weather.list[4].weather[0].description}</div>
+      <div class="hi-low">${Math.round(weather.list[4].main.temp_min)} / ${Math.round(weather.list[0].main.temp_max)}</div>
+    </div>
+  
+  `
+
+  // let temp = document.querySelector('.days .zero .temp')
+  // temp.innerHTML = `${Math.round(weather.list[0].main.temp)}<span>°F</span>`;
+
+  // let weather_el = document.querySelector('.current .weather');
+  // weather_el.innerText = weather.weather[0].main;
+
+  // let hilow = document.querySelector('.hi-low');
+  // hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
+}
+
+function displayResults(weather) {
   let city = document.querySelector('.location .city');
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
